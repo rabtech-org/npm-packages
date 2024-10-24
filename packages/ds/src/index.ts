553237450b0f9fad7a7dfd706d645de91f9192f4
rabezzano.francesco <rabezzano.francesco@gmail.com>
2024-10-24 21:08:04 +0200

    fix: update exports

--- packages/ds/src/index.ts
+++ packages/ds/src/index.ts
@@ -1,12 +1,5 @@
-export {
-    alpha,
-    createFilterOptions,
-    createTheme,
-    CssBaseline,
-    debounce,
-    ThemeProvider,
-    tooltipClasses,
-    useMediaQuery,
-    useTheme,
-} from '@mui/material';
-export { red, green, blue, grey } from '@mui/material/colors';
+export * from './components';
+export * from './types';
+export * from './utils';
+export * from './icons';
+export * from './themes';
